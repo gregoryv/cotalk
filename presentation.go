7105dcf7efec32d0a1ce3ede2bf30ded59bb0718
grevin <gregory.vincic@assaabloy.com>
2025-01-09 12:51:19 +0100

    Use type Deck

--- presentation.go
+++ presentation.go
@@ -3,7 +3,6 @@ package main
 import (
 	"embed"
 	"fmt"
-	"os"
 	"strings"
 
 	. "github.com/gregoryv/web"
@@ -11,19 +10,102 @@ import (
 )
 
 func main() {
-	page := Presentation().Page()
-	if err := page.SaveAs("docs/index.html"); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	d := Deck{
+		Title:     "Go Concurrency",
+		Author:    "Gregory Vinčić",
+		AutoCover: true,
+		cover:     Img(Src("cotalk.png")),
+		AutoTOC:   true,
 	}
-}
+	d.Style(".header", "background-color: #f6f5f4",
+		"border-bottom: 1px inset #000",
+	)
+	d.Style(".page .footer", "bottom: 1vh") // slightly up
+	d.Style(".right>*, .right .shell",
+		"margin-right: 1em",
+	)
 
-func Presentation() *deck {
-	d := newDeck()
-	d.Title = "Go concurrency"
-	d.Styles = append(d.Styles,
-		theme(),
-		highlightColors(),
+	// center toc if short titles
+	d.Style(".toc",
+		"font-size: 0.9em",
+	)
+	d.Style(".shell",
+		"padding: 1em",
+		"border-radius: 10px",
+		"overflow: wrap",
+	)
+	d.Style(".dark",
+		"background-color: #2e2e34",
+		"color: aliceblue",
+	)
+	d.Style("nav",
+		"column-count: 2", // Fix this at the end with a manual toc
+		"font-size: 0.8em",
+	)
+	d.Style(".srcfile ol",
+		"padding-right: 40px",
+	)
+	d.Style(".shell",
+		"font-size: 1.5vw",
+	)
+	d.Style("p>a",
+		"text-decoration: underline",
+	)
+	d.Style("quote",
+		"display: inline-block",
+		"font-style: italic",
+		"padding-left: 2vw",
+		"padding-right: 2vw",
+		"font-size: 1.5vw",
+	)
+	d.Style("quote>a",
+		"font-size: 1vw",
+		"float: right",
+	)
+	d.Style("quote.small",
+		"font-size: 1vw",
+	)
+
+	d.Style(".pictogram",
+		"display: block",
+		"width: 400px",
+		"text-align: center",
+		"padding: 1em 1em",
+		"margin: 1em auto",
+	)
+	d.Style(".pictogram label",
+		"font-style: italic",
+		"font-size: 0.6em",
+	)
+	d.Style(".srcfile",
+		"font-size: 1.3vw",
+	)
+	d.Style(".filename",
+		"display: block",
+		"font-size: 0.8vw",
+		"text-align: right",
+		"margin-top: 2em",
+		"margin-bottom: -2em",
+	)
+	d.Style(".icons",
+		"column-count: 3",
+		"list-style-type: none",
+	)
+
+	d.Style(".small *",
+		"font-size: 0.9vw",
+	)
+	d.Style(".small .filename",
+		"margin-bottom: -1.5em",
+	)
+	d.Style(".stop>img",
+		"text-align: center",
+		"float:left",
+		"margin-right: 20px",
+	)
+	d.Style(".stop>p",
+		"padding-top: 50px",
+		"color: red",
 	)
 
 	alg := &algorithms{
@@ -31,7 +113,7 @@ func Presentation() *deck {
 		atLine:  9,
 	} // 8 where first func starts
 
-	d.Slide(
+	d.NewCard(
 		H2("Go concurrency design"),
 		A(Href("#2"), Img(Src("main.png"))),
 		Br(), Br(), Br(),
@@ -39,7 +121,7 @@ func Presentation() *deck {
 		Br(), Br(), Br(),
 	)
 
-	d.Slide(
+	d.NewCard(
 		H2("Content"),
 		Table(Tr(Td(
 			Ul(Class("group"),
@@ -75,7 +157,7 @@ func Presentation() *deck {
 		),
 	)
 
-	d.Slide(H2("Background and history"),
+	d.NewCard(H2("Background and history"),
 		Table(
 			Tr(
 				Td(Img(Src("th_small.jpg"))),
@@ -102,14 +184,14 @@ func Presentation() *deck {
 		),
 	)
 
-	d.Slide(H2("Goroutines"),
+	d.NewCard(H2("Goroutines"),
 		A(Attr("target", "_blank"),
 			Href("https://go.dev/tour/concurrency/1"),
 			Img(Class("center"), Src("gotour_concurrency_1.png")),
 		),
 	)
 
-	d.Slide(H2("channels"),
+	d.NewCard(H2("channels"),
 		A(Attr("target", "_blank"),
 			Href("https://go.dev/tour/concurrency/2"),
 			Img(Class("center"), Src("gotour_concurrency_2.png")),
@@ -118,11 +200,11 @@ func Presentation() *deck {
 
 	// packages
 	// ----------------------------------------
-	d.Slide(H2("package sync"),
+	d.NewCard(H2("package sync"),
 		godoc("sync", ""),
 	)
 
-	d.Slide(H2("package context"),
+	d.NewCard(H2("package context"),
 		Pre(highlightGoDoc(`package context
 
 ...
@@ -147,7 +229,7 @@ processes and APIs, not for passing optional parameters to functions.
 		godoc("context", "-short"),
 	)
 
-	d.Slide(H2("go test -bench"),
+	d.NewCard(H2("go test -bench"),
 		Table(Class("twocolumn"), Tr(Td(
 			load("benchx_test.go"),
 		), Td(
@@ -160,11 +242,11 @@ processes and APIs, not for passing optional parameters to functions.
 
 	// Problem definition
 	// ----------------------------------------
-	d.Slide(H2("Problem"),
+	d.NewCard(H2("Problem"),
 		load("problem.go"),
 	)
 
-	d.Slide(H2("The letter challenge"),
+	d.NewCard(H2("The letter challenge"),
 		Table(Class("twocolumn"), Tr(Td(
 			LoadLines("letters.go", 13, 38),
 		), Td(
@@ -172,25 +254,25 @@ processes and APIs, not for passing optional parameters to functions.
 		))),
 	)
 
-	d.Slide(H2("Verification"),
+	d.NewCard(H2("Verification"),
 		P(`Each algorithm in these examples is tested like this`),
 		LoadLines("alg_test.go", 10, 24),
 	)
 
-	d.Slide(H2("Sequential"),
+	d.NewCard(H2("Sequential"),
 		P("Simple implementation though very low performance"),
 		alg.next(),
 	)
 
 	// Concurrent
 	// ----------------------------------------
-	d.Slide(H2("Concurrent using sync.WaitGroup"),
+	d.NewCard(H2("Concurrent using sync.WaitGroup"),
 		alg.next(
 			P("Why does it fail?"),
 		),
 	)
 
-	d.Slide(H2("Arguments are evaluated at calltime"),
+	d.NewCard(H2("Arguments are evaluated at calltime"),
 		alg.next(
 			P(
 				"You might get a different result; why does it still fail? and can the tooling help identify the problem, try ",
@@ -201,17 +283,17 @@ processes and APIs, not for passing optional parameters to functions.
 		),
 	)
 
-	d.Slide(H2("Protect concurrent writes with sync.Mutex"),
+	d.NewCard(H2("Protect concurrent writes with sync.Mutex"),
 		alg.next(
 			P("Why does it fail?"),
 		),
 	)
 
-	d.Slide(H2("Sort results"),
+	d.NewCard(H2("Sort results"),
 		alg.next(),
 	)
 
-	d.Slide(H2("Improved performance paid with complexity"),
+	d.NewCard(H2("Improved performance paid with complexity"),
 
 		P(`Comparing the sequential working algorithm to the working
 		concurrent one, tests reveal a substantial improvement.`),
@@ -222,25 +304,25 @@ processes and APIs, not for passing optional parameters to functions.
 		),
 	)
 
-	d.Slide(H2("Using channel"),
+	d.NewCard(H2("Using channel"),
 		alg.next(),
 	)
 
-	d.Slide(H2("Correct order using channel"),
+	d.NewCard(H2("Correct order using channel"),
 		alg.next(
 			P("There is still a bug in this code, do you see it?"),
 		),
 	)
-	d.Slide(H2("Clean up resources"),
+	d.NewCard(H2("Clean up resources"),
 		alg.next(),
 	)
-	d.Slide(H2("Interrupt"),
+	d.NewCard(H2("Interrupt"),
 		alg.next(),
 	)
-	d.Slide(H2("Respect context cancellation"),
+	d.NewCard(H2("Respect context cancellation"),
 		alg.next(),
 	)
-	d.Slide(H2("Compare all"),
+	d.NewCard(H2("Compare all"),
 
 		P(`In this example using channels and sync package primitives
 		seem to yield more or less the same result. There performance
@@ -253,7 +335,7 @@ processes and APIs, not for passing optional parameters to functions.
 		),
 	)
 
-	d.Slide(H2("Go concurrency design summary"),
+	d.NewCard(H2("Go concurrency design summary"),
 
 		Ul(
 			Li("concurrency is difficult to get right; even in Go"),
@@ -263,7 +345,7 @@ processes and APIs, not for passing optional parameters to functions.
 		),
 		//
 	)
-	return d
+	d.Document().SaveAs("docs/index.html")
 }
 
 func shell(cmd, filename string) *Element {
@@ -311,266 +393,4 @@ func (a *algorithms) next(extra ...interface{}) *Element {
 		),
 		),
 	)
-}
-
-// ----------------------------------------
-
-// newDeck returns a deck with default styling and navigation on bottom
-func newDeck() *deck {
-	return &deck{
-		Styles: []*CSS{},
-	}
-}
-
-// Had this idea of a deck of slides; turned out to be less
-// useful. Leaving it here for now.
-type deck struct {
-	Title  string // header title
-	Slides []*Element
-	Styles []*CSS // first one is the deck default styling
-}
-
-// Slide appends a new slide to the deck. elements can be anything
-// supported by the web package.
-func (d *deck) Slide(elements ...interface{}) {
-	d.Slides = append(d.Slides, Wrap(elements...))
-}
-
-// Page returns a web page ready for use.
-func (d *deck) Page() *Page {
-	styles := Style()
-	for _, s := range d.Styles {
-		styles.With(s)
-	}
-	body := Body()
-	nav := &navbar{current: 1, max: len(d.Slides)}
-	for i, content := range d.Slides {
-		j := i + 1
-		id := fmt.Sprintf("%v", j)
-		slide := Div(Class("slide"), Id(id))
-
-		slide.With(
-			Header(content.Children[0]),
-		)
-		div := Div(Class("content"))
-		div.With(content.Children[1:]...)
-		slide.With(div, nav.next())
-		body.With(slide)
-	}
-	body.With(Script(enhancejs))
-	return NewFile("index.html",
-		Html(
-			Head(
-				Title(d.Title),
-				Meta(Name("viewport"), Content("width=device-width, initial-scale=1")),
-				styles,
-			),
-			body,
-		),
-	)
-}
-
-type navbar struct {
-	max     int // number of slides
-	current int // current slide
-}
-
-// BuildElement is called at time of rendering
-func (b *navbar) next() *Element {
-	ul := Ul()
-	groupDivider := map[int]bool{
-		9:  true,
-		13: true, // concurrent
-		17: true, // channels
-	}
-
-	for i := 0; i < b.max; i++ {
-		j := i + 1
-		hash := fmt.Sprintf("#%v", j)
-		li := Li(A(Href(hash), j))
-		if j == b.current {
-			li.With(Class("current"))
-		}
-		if groupDivider[j] {
-			ul.With(Li(" | "))
-		}
-		ul.With(li)
-	}
-	b.current++
-	return Nav(ul)
-}
-
-// ----------------------------------------
-
-func theme() *CSS {
-	css := NewCSS()
-	css.Import("https://fonts.googleapis.com/css?family=Inconsolata|Source+Sans+Pro")
-
-	css.Style("html, body",
-		"font-family: 'Source Sans Pro', sans-serif",
-		"margin: 0 0",
-		"padding: 0 0",
-	)
-	css.Style(".slide",
-		//"border: 3px solid magenta",
-		"margin: 0 0",
-		"padding: 0 0",
-		"text-align: center",
-		"height: 100vh",
-	)
-	bg := "#cde9e9"
-	css.Style(".slide header",
-		"display: block",
-		"border: 1px solid "+bg, // needed to make it without margins ?!
-		"margin: 0 0",
-		"background-color: "+bg,
-		"vertical-align: center",
-		"height: 10%",
-	)
-
-	css.Style(".slide .content",
-		// "border: 3px solid red",
-		"margin: 0 0",
-		"padding: 1% 1%",
-		"height: 80%",
-		"overflow: auto",
-	)
-	css.Style(".slide ul, p, pre",
-		"text-align: left",
-	)
-
-	// navbar
-	css.Style(".slide nav",
-		// "border: 3px solid green",
-		"display: block",
-		"margin: 0 0",
-		"padding: 0 0",
-		"text-align: center",
-		"clear: both",
-		"width: 100%",
-		"height: 5%",
-	)
-	css.Style(".slide nav ul",
-		"list-style-type: none",
-		"margin: 0 0",
-		"padding: 0 0",
-		"text-align: center",
-	)
-	css.Style(".slide nav ul li",
-		"margin: 0 1px",
-		"display: inline",
-	)
-	css.Style(".slide nav ul li.current a, .slide nav ul li:hover a",
-		"background-color: #e2e2e2",
-		"border-radius: 5px",
-	)
-	css.Style("nav a:link, nav a:visited",
-		"color: #727272",
-		"padding: 3px 5px",
-		"text-decoration: none",
-		"cursor: pointer",
-	)
-
-	// goish colors
-	css.Style("a:link, a:visited",
-		"color: #007d9c",
-		"text-decoration: none",
-	)
-	css.Style("a:hover",
-		"text-decoration: underline",
-	)
-	css.Style(".header",
-		"width: 100%",
-		"border-bottom: 1px solid #727272",
-		"text-align: right",
-		"margin-top: -2em",
-		"margin-bottom: 1em",
-	)
-	css.Style("h1, h2, h3",
-		"text-align: center",
-	)
-	css.Style(".twocolumn",
-		// "border: 3px solid brown",
-		"width: 100%",
-	)
-	css.Style(".srcfile",
-		"margin-top: 1.6em",
-		"margin-bottom: 1.6em",
-		"background-image: url(\"printout-whole.png\")",
-		"background-repeat: repeat-y",
-		"padding-left: 36px",
-		"background-color: #fafafa",
-		"tab-size: 4",
-		"-moz-tab-size: 4",
-		"min-width: 35vw",
-	)
-
-	css.Style(".srcfile code",
-		"padding: .6em 0 2vh 0",
-		"background-image: url(\"printout-whole.png\")",
-		"background-repeat: repeat-y",
-		"background-position: right top",
-		"display: block",
-		"text-align: left",
-		"font-family: Inconsolata",
-		"overflow: wrap",
-	)
-	css.Style(".srcfile code .line", // each line
-		"display: block",
-		"width: 98%",
-		"clear: both",
-		"margin-bottom: -1.5vh",
-	)
-	css.Style(".srcfile code span:hover", // each line
-		"background-color: #b4eeb4",
-	)
-
-	css.Style(".srcfile code i",
-		"font-style: normal",
-		"color: #a2a2a2",
-	)
-
-	// toc
-	css.Style("li.h3",
-		"margin-left: 2em",
-	)
-	css.Style(".shell",
-		"padding: 1em",
-		"border-radius: 10px",
-		"min-width: 40vw",
-		"overflow: wrap",
-	)
-	css.Style(".dark",
-		"background-color: #2e2e34",
-		"color: aliceblue",
-	)
-	css.Style(".light",
-		"background-color: #ffffff",
-		"color: #3b2616",
-	)
-	css.Style("img.center",
-		"display: block",
-		"margin-left: auto",
-		"margin-right: auto",
-	)
-	css.Style("img.left",
-		"float: left",
-		"margin-right: 2em",
-	)
-	css.Style(".group",
-		"text-align: left",
-		"margin-right: 3em",
-	)
-	css.Style(".group:first-child",
-		"margin-left: 5vw",
-	)
-
-	css.Style("td",
-		"vertical-align: top",
-	)
-	css.Style("td:nth-child(2)",
-		"padding-left: 2em",
-	)
-
-	return css
 }
